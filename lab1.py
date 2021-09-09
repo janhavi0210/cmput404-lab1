49e3b4c18ae532d56160fd5c91a98ee10a59216d
janhavi0210 <75977053+janhavi0210@users.noreply.github.com>
2021-09-08 19:52:57 -0600

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,4 +1,8 @@
 import requests
 print(requests.__version__)
 r=requests.get("https://www.google.com/")
+print("Google Homepage:")
 print(r.text)
+script_clone=request.get("https://raw.githubusercontent.com/janhavi0210/cmput404-lab1/master/lab1.py")
+print("Downloaded Source Code:")
+print(script_clone.text)
